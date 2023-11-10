b66452cc2672f8d494fd093c22c41235bf22419f
ephychime@gmail.com <ephy@Ephys-MacBook-Air.local>
2023-11-09 19:01:02 -0500

    Updated text

--- githubflow/src/App.js
+++ githubflow/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I am updating this text
+         I have updated this text 
         </p>
         <a
           className="App-link"
